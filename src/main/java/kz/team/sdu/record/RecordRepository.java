b25a4ca2eff4872fe12fe9afd6408e6956419f70
angry-boy-coder <kongratbaev.77.7@gmail.com>
2022-02-24 10:39:15 +0600

    Models

--- src/main/java/kz/team/sdu/record/RecordRepository.java
+++ src/main/java/kz/team/sdu/record/RecordRepository.java
@@ -3,6 +3,6 @@ package kz.team.sdu.record;
 import kz.team.sdu.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecordRepository extends JpaRepository<User, Integer> {
+public interface RecordRepository extends JpaRepository<Record, Integer> {
 
 }
