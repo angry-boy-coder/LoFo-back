3c4230f50f4f65bd0c212e9404562e65b87409ed
KONGRATBAYEV NURZHAN <kongratbaev.77.7@gmail.com>
2022-03-03 11:23:12 +0300

    Record Controller(create edit)

--- src/main/java/kz/team/sdu/record/RecordController.java
+++ src/main/java/kz/team/sdu/record/RecordController.java
@@ -4,11 +4,13 @@ import kz.team.sdu.system.Status;
 import kz.team.sdu.user.User;
 import kz.team.sdu.user.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -33,6 +35,37 @@ public class RecordController {
     public Optional<Record> getRecord(@PathVariable("id") Integer id)  {
         return recordRepository.findById(id);
     }
+
+
+    @PostMapping("/create")
+    public ResponseEntity createRecord(@Valid @RequestBody Record record)  {
+        Status status = new Status();
+        recordRepository.save(record);
+        status.status = 1;
+        status.message = "Created";
+        return new ResponseEntity(status, HttpStatus.OK);
+    }
+
+    @PostMapping("/edit/{id}")
+    public ResponseEntity editRecord(
+            @PathVariable("id") Integer id,
+            @Valid @RequestBody Record editRecord
+    )  {
+        Status status = new Status();
+        Optional<Record> record = recordRepository.findById(id);
+        if(record.isPresent()){
+            record.get().setTitle(editRecord.getTitle());
+            record.get().setCategory(editRecord.getCategory());
+            record.get().setDescription(editRecord.getDescription());
+            record.get().setPlace(editRecord.getPlace());
+            record.get().setPlace(editRecord.getPlace());
+            recordRepository.save(record.get());
+            status.status = 1;
+            status.message = "Saved";
+        }
+        return new ResponseEntity(status, HttpStatus.OK);
+    }
+
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity removeRecord(@PathVariable("id") Integer id)  {
